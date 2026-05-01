f355149c24d35e69dfe0745c8e75648127661874
Vercel <vercel[bot]@users.noreply.github.com>
2026-05-01 20:22:25 +0000

    Install Vercel Web Analytics
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- meu-site/src/App.tsx
+++ meu-site/src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Sobre from './components/Sobre'
 import Projetos from './components/Projetos'
 import Contato from './components/Contatos'
+import { Analytics } from '@vercel/analytics/react'
 function App() {
   
   return (
@@ -11,6 +12,7 @@ function App() {
       <Sobre />
       <Projetos />
       <Contato />
+      <Analytics />
     </div>
   )
 };
